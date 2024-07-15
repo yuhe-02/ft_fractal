867cdd2e5aaf85289afd0c843c3a87a89ecbd107
Yuhei Yamasaki <yyamasak@c2r9s1.42tokyo.jp>
2024-07-15 15:55:37 +0900

    test

---
+++ README.md
@@ -0,0 +1,66 @@
+[日本語](#日本語)
+
+# Minilibx Sample for Mac
+This sample is based on the code from the following site:
+
+https://harm-smits.github.io/42docs/libs/minilibx
+
+This sample project implements simple drawing and event hooks.
+
+![output2](https://github.com/ssmyg/minilibx_sample/assets/10322119/9f20837c-5217-452a-b5d2-9c55de39f4af)
+
+
+# Usage
+Clone this project and build.
+```
+$ git clone https://github.com/ssmyg/minilibx_sample.git
+$ cd minilibx_sample
+$ make
+$ ./sample
+```
+
+# Extra
+I wrote an article about fract'ol. Please refer to it if you like.
+
+[Fract'ol - How to zoom to the limit and smooth operation](https://zenn.dev/grigri_grin/articles/7541be0ba6e725)
+
+https://www.youtube.com/watch?v=2Zf4xkm4hhI
+
+![Mandelbrot Set](https://github.com/ssmyg/minilibx_sample/assets/10322119/c56287bb-9ece-4f49-88c1-a8654fe45788)
+
+-------------------
+
+# 日本語
+# Minilibx Sample for Mac
+このサンプルは下記サイトのコードを参考にしています。
+
+https://harm-smits.github.io/42docs/libs/minilibx
+
+簡単な描画とイベントフックを一通り実装しています。
+
+![output2](https://github.com/ssmyg/minilibx_sample/assets/10322119/9f20837c-5217-452a-b5d2-9c55de39f4af)
+
+
+# 使用方法
+プロジェクトをクローンした後、ビルドすると、sampleという実行ファイルが作成されます。
+
+```
+$ git clone https://github.com/ssmyg/minilibx_sample.git
+$ cd minilibx_sample
+$ make
+$ ./sample
+```
+
+# おまけ
+fract'olをする場合、記事を書いたのでもしよければ参考にして下さい。
+
+[fract-ol 〜サクサク動いて限界まで拡大する方法〜](https://zenn.dev/grigri_grin/articles/5d201067dedfdb)
+
+動画
+
+https://www.youtube.com/watch?v=2Zf4xkm4hhI
+
+![Mandelbrot Set](https://github.com/ssmyg/minilibx_sample/assets/10322119/c56287bb-9ece-4f49-88c1-a8654fe45788)
+
+
+
---
+++ src/main.c
@@ -0,0 +1,108 @@
+
+#include "../minilibx-linux/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+
+typedef struct	s_data {
+    void    *mlx;
+    void    *win;
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+    double zoom;
+    double offsetX;
+    double offsetY;
+}				t_data;
+
+#define WIDTH 1600
+#define HEIGHT 1600
+#define ACRAC 1000
+
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int close_window(t_data *data) {
+    mlx_destroy_image(data->mlx, data->img);
+    mlx_destroy_window(data->mlx, data->win);
+    exit(0);
+}
+
+int key_hook(int keycode, t_data *data) {
+    if (keycode == 65307) // ESC key
+        close_window(data);
+    return (0);
+}
+
+void draw_julia_set(t_data *img) {
+
+	// C の実部と虚部
+	double	cx = -0.3;
+	double	cy = -0.63;
+
+    // フラクタルの描画処理
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            double zx = (x - (WIDTH / 2)) / (0.5 * img->zoom * WIDTH) + img->offsetX;
+            double zy = (y - (HEIGHT / 2)) / (0.5 * img->zoom *HEIGHT) + img->offsetY;
+            int i = 0;
+            while (zx * zx + zy * zy < 4 && i < ACRAC) {
+                double tmp = zx * zx - zy * zy + cx;
+                zy = 2.0 * zx * zy + cy;
+                zx = tmp;
+                i++;
+            }
+            if (i == ACRAC)
+                i = 0;
+            int color = i * 10000;
+            my_mlx_pixel_put(img, x, y, color);
+        }
+    }
+    mlx_put_image_to_window(img->mlx, img->win, img->img, 0, 0);
+}
+
+
+int mouse_hook(int button, int x, int y, void *param) {
+    t_data *img = (t_data *)param;
+    if (button == 4) { // マウスホイールアップ（ズームイン）
+        img->zoom *= 1.2;
+    } else if (button == 5) { // マウスホイールダウン（ズームアウト）
+        img->zoom /= 1.2;
+    }
+    mlx_clear_window(img->mlx, img->win);
+    draw_julia_set(img);
+    return 0;
+}
+
+int	main(void)
+{    // MiniLibX の初期化
+    void	*mlx = mlx_init();
+    void	*win = mlx_new_window(mlx, WIDTH, HEIGHT, "Julia Set");
+    t_data	img;
+
+    img.mlx = mlx;
+    img.win = win;
+    img.img = mlx_new_image(img.mlx, WIDTH, HEIGHT);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+    img.zoom = 1.0;
+    img.offsetX = 0.0;
+    img.offsetY = 0.0;
+
+	draw_julia_set(&img);
+
+    mlx_hook(img.win, 17, 0, close_window, &img);
+    mlx_key_hook(img.win, key_hook, &img);
+    mlx_mouse_hook(img.win, mouse_hook, &img);
+    mlx_loop(img.mlx);
+
+    return (0);
+}
