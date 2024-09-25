b592cfac869f435bc2a6074b0618c7afd804f84d
yuhe-02 <domineter4649@gmail.com>
2024-09-25 15:18:54 +0000

    update today's progress

--- src/draw_fractal/ft_mandel.c
+++ src/draw_fractal/ft_mandel.c
@@ -6,7 +6,7 @@
 /*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 23:42:50 by yyamasak          #+#    #+#             */
-/*   Updated: 2024/09/25 23:53:50 by yyamasak         ###   ########.fr       */
+/*   Updated: 2024/09/25 15:16:20 by yyamasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ t_coord	*calc_mandelbrot_set(t_data *img, int x, int y)
 	cd = (t_coord *)malloc(sizeof(t_coord));
 	if (!cd)
 		exit(1);
-	cd->zx = 0;
-	cd->zy = 0;
+	cd->zx = img->param1;
+	cd->zy = img->param2;
 	cd->cx = (x - (WIDTH / 2)) / (0.5 * img->zoom * WIDTH) + img->offset_x;
 	cd->cy = (y - (HEIGHT / 2)) / (0.5 * img->zoom * HEIGHT) + img->offset_y;
 	return (cd);
